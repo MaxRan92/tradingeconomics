96aac0303dadc90cff1fb212921845656d4fc9c2
Massimo Ranalli <ranallimax@gmail.com>
2022-11-01 10:14:26 +0000

    Add cloudinary

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://cppcjqtjgxrkwm:020b620eb53c6656a00f01cb9aba927729ce9fccd0dd5ed57ed81bd78219e2f0@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d3ktqhbdrcn4kd"
 os.environ["SECRET_KEY"] = "secret_key"
+os.environ["CLOUDINARY_URL"] = "cloudinary://567951223929236:tdmGw7I4hxTUvckv_8A9GUworj0@dt5umjifx"
