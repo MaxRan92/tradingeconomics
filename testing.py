5604783874371e5467c078da7e9b502d2a06b36b
Massimo Ranalli <ranallimax@gmail.com>
2022-11-01 22:05:52 +0000

    Tested real interest rate download

--- testing.py
+++ testing.py
@@ -1,8 +1,17 @@
+import requests
 import tradingeconomics as te
+import pandas as pd
+import json
 
 def get_data():
     te.login()
-    indicators = te.getIndicatorData(country=['mexico', 'sweden'], output_type='df')
+    indicators = te.getWBIndicator(series_code = 'fr.inr.rinr', output_type='df')
+    #with pd.option_context('display.max_rows', None, 'display.max_columns', None):   more options can be specified also
     print(indicators)
+    # URL ='https://api.tradingeconomics.com/country/Mexico?c=guest:guest&f=json'
+    # df = pd.read_json(URL)
+    # print(df)
+    
+
 
 get_data()
